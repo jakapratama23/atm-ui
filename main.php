03354838b252b6a2a08a66a09811de3a77483c11
jakapratama23 <38776574+jakapratama23@users.noreply.github.com>
2023-03-07 12:56:40 +0700

    Update main.php

--- main.php
+++ main.php
@@ -3,3 +3,22 @@
 declare(strict_types=1);
 
 namespace Main:
+
+use pocketmine\entity\EntityDataHelper;
+use pocketmine\entity\EntityFactory;
+use pocketmine\entity\Human;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\permission\Permission;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+use pocketmine\utils\TextFormat;
+use pocketmine\world\World;
+
+class Main extends PluginBase implements Listener
+{
+  
+  // soon
